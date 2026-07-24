ee8f0fcb640b25731658ef9e5fc20714493d03af
Gaurav Mishra <gauravmishra271865@gmail.com>
2026-07-25 01:01:00 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/gaurav/aiproductivity/AiProductivityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.aiproductivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProductivityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProductivityBackendApplication.class, args);
+	}
+
+}
