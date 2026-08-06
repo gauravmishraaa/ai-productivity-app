39917291081fa94b8a2f0d478db82a409e5c37a2
Gaurav Mishra <gauravmishra271865@gmail.com>
2026-08-07 01:06:20 +0530

    Restructuring

--- src/main/java/com/gaurav/aiproductivity/AiProductivityBackendApplication.java
+++ src/main/java/com/gaurav/aiproductivity/AiProductivityBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiProductivityBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiProductivityBackendApplication.class, args);
+		SpringApplication.run(AiProductivityBackendApplication.class, args)
 	}
 
 }
